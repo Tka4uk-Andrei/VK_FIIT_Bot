9c7aadf3462090acae7eeb5f7ba13ffb670cc3cf
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-02-07 22:36:08 +0300

    add user notification

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ VK_API_KEY = "28f2396ab6b6ca0d2cbc63caec4561b3b3005aeab6d89f0432578344fe0b181409
 VK_VERSION = "5.103"
 VK_GROUP_ID = "club191500224"
 
+# Path to data files
 SUBSCRIPTION_ID_FILE_NAME = "data/subscribedIds.txt"
+LATEST_MSG_ID_FILE_NAME = "data/latest_msg_id.txt"
 
 print ('Launching...')
 
@@ -20,11 +22,17 @@ with open(SUBSCRIPTION_ID_FILE_NAME, "r") as subscribedIds:
     for line in subscribedIds:
         connectedIds.append(int(line))
 
+messageCounter = 0
+# read message id from where you should start
+with open(LATEST_MSG_ID_FILE_NAME, "r") as msgId:
+    for line in msgId:
+        messageCounter = int(line)
 
 print ('Server started listening')
 for event in longpoll.listen():
     print ('Event handling start...')
     if event.type == VkBotEventType.MESSAGE_NEW:
+        # handle message from subscribed user
         if event.from_user:
             # receive users id
             fromId = event.obj.message[u'from_id']
@@ -38,14 +46,17 @@ for event in longpoll.listen():
                     for userId in connectedIds:
                         subscribedIds.write(str(userId))
 
-            # sessionApi.message.send()
+            # mark message as read
+            sessionApi.messages.markAsRead(start_message_id=[event.message[u'id']], peer_id=event.message[u'peer_id'])
 
+        # handle message from
         elif event.from_chat:
             print ('message from chat')
-            # vk.messages.send(
-            #     chat_id=event.chat_id,
-            #     message='Hello chat and world'
-            # )
+            # notify subscribed users
+            for userId in connectedIds:
+                sessionApi.messages.send(peer_id=userId, random_id=messageCounter, message='Check conference')
+                
+
     print ('Event handling ended')
 
 print ('The End')
