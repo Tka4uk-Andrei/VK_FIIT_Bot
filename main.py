451357a0c808578ce2eaf757705a1cd4267a9a73
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-02-08 18:16:20 +0300

    first try to add attachment support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import requests
 
 # Global constants
 VK_API_KEY = "28f2396ab6b6ca0d2cbc63caec4561b3b3005aeab6d89f0432578344fe0b181409b77e043302013be1655"
@@ -9,6 +10,32 @@ VK_GROUP_ID = "club191500224"
 # Path to data files
 SUBSCRIPTION_ID_FILE_NAME = "data/subscribedIds.txt"
 LATEST_MSG_ID_FILE_NAME = "data/latest_msg_id.txt"
+
+
+def reform_attachments(attachments):
+    attachment_arr = []
+    if attachments.__len__() > 0:
+        for attachment in attachments:
+            attachment_arr.append(str(attachment[u'type'] + \
+                             str(attachment[attachment[u'type']][u'owner_id']) + '_' + \
+                             str(attachment[attachment[u'type']][u'id']) + '_' + \
+                             attachment[attachment[u'type']][u'access_key']))
+    return attachment_arr
+
+
+def reform_forward_msg(forward_msgs):
+    forward_msg_str = ''
+    if forward_msgs.__len__() > 0:
+        for i in range(forward_msgs.__len__() - 1):
+            # for attachment in attachments:
+            forward_msg = forward_msgs[i]
+            forward_msg_str = forward_msg_str + str(forward_msg[u'id']) + ','
+
+        forward_msg = forward_msgs[forward_msgs.__len__() - 1]
+        forward_msg_str = forward_msg_str + str(forward_msg[u'id'])
+
+    return forward_msg_str
+
 
 print ('Launching...')
 
@@ -49,13 +76,21 @@ for event in longpoll.listen():
             # mark message as read
             sessionApi.messages.markAsRead(start_message_id=[event.message[u'id']], peer_id=event.message[u'peer_id'])
 
-        # handle message from
-        elif event.from_chat:
+        # handle message from group chat
+        elif event.from_chat and event.message.text != '' and event.message.text[0:2] == '//':
             print ('message from chat')
             # notify subscribed users
             for userId in connectedIds:
-                sessionApi.messages.send(peer_id=userId, random_id=messageCounter, message='Check conference')
-                
+                # switch 'peer_id' to broadcast list
+                # forward messages not supported by VK API for bots??? W H A T !? :\
+                print(reform_attachments(event.message.attachments))
+                print(reform_forward_msg(event.message.fwd_messages))
+                sessionApi.messages.send(peer_id=userId,
+                                         random_id=0,
+                                         message=event.message.text,
+                                         attachment=reform_attachments(event.message.attachments))
+    #                   ,
+    #                                                forward_messages=reform_forward_msg(event.message.fwd_messages))
 
     print ('Event handling ended')
 
