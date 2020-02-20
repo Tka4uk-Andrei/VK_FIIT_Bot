715ca94a91c3fde41c6a5c4cc4f77d2f526b2de1
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-02-20 23:39:46 +0300

    moved loading configuration to separated class

--- config/legacy_configuration_loader.py
+++ config/legacy_configuration_loader.py
@@ -1,47 +1,48 @@
-class ConfigurationType:
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+class LegacyConfigurationLoader:
 
     def __init__(self):
-        self.type = ""
-        self.set_legacy()
+        # Paths to configuration files
+        self.__SUBSCRIPTION_ID_FILE_NAME = "data/subscribed_ids.txt"
+        self.__LATEST_MSG_ID_FILE_NAME = "data/latest_msg_id.txt"
+        self.__CONFIG_FILE_NAME = "data/config.txt"
 
-    def set_json(self):
-        self.type = self.json()
+        # read api_key and group_id
+        with open(self.__CONFIG_FILE_NAME, "r") as configs:
+            lines = configs.read().splitlines()
+            self.__api_key = lines[0]
+            self.__group_id = int(lines[2])
 
-    def set_legacy(self):
-        self.type = self.legacy()
+        # read subscriber_ids
+        with open(self.__SUBSCRIPTION_ID_FILE_NAME, "r") as user_ids:
+            self._subscriber_ids = [int(lne) for lne in user_ids]
 
-    def get_config_type(self):
-        return self.type
-
-    @staticmethod
-    def legacy():
-        return "legacy"
-
-    @staticmethod
-    def json():
-        return "json"
+        # read message id from where you should start (not used information)
+        # why it's required?
+        with open(self.__LATEST_MSG_ID_FILE_NAME, "r") as msg_id:
+            self._message_ids = [int(lne) for lne in msg_id]
 
 
-class LegacyConfigurator:
+    def get_subscriber_ids(self):
+        return self._message_ids
 
-    def __init__(self, config_type):
-        # Path to data files
-        SUBSCRIPTION_ID_FILE_NAME = "data/subscribedIds.txt"
-        LATEST_MSG_ID_FILE_NAME = "data/latest_msg_id.txt"
-        CONFIG_FILE_NAME = "data/config.txt"
 
-        # Read global constants
-        with open(CONFIG_FILE_NAME, "r") as configs:
-            VK_API_KEY = configs.readline()[0:-1]
-            VK_VERSION = configs.readline()[0:-1]
-            VK_GROUP_ID = configs.readline()
+    def save_subscriber_ids(self, ids = self._message_ids):
+        self._message_ids = ids
+        with open(self.__SUBSCRIPTION_ID_FILE_NAME, "a") as f:
+            for i in self._message_ids:
+                f.write(f'{i}')
 
-        # read added early ids from file
-        with open(SUBSCRIPTION_ID_FILE_NAME, "r") as subscribedIds:
-            for line in subscribedIds:
-                connectedIds.append(int(line))
 
-        # read message id from where you should start
-        with open(LATEST_MSG_ID_FILE_NAME, "r") as msgId:
-            for line in msgId:
-                messageCounter = int(line)
+    def get_api_key(self):
+        return self.__api_key
+    
+
+    def add_group_id(self, id):
+        self._subscriber_ids.append(id)
+
+
+    def get_group_id(self):
+        return self.__group_id
--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import threading
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-# Path to data files
-SUBSCRIPTION_ID_FILE_NAME = "data/subscribed_ids.txt"
-LATEST_MSG_ID_FILE_NAME = "data/latest_msg_id.txt"
-CONFIG_FILE_NAME = "data/config.txt"
+from config.legacy_configuration_loader import LegacyConfigurationLoader
+
 APP_LOGGER = logging
 
 
@@ -38,31 +36,12 @@ def reform_forward_msg(forward_msgs):
     return forward_msg
 
 
-def load_settings():
-    with open(CONFIG_FILE_NAME, "r") as configs:
-        lines = configs.read().splitlines()
-        api_key = lines[0]
-        group_id = int(lines[2])
-
-    vk_session = vk_api.VkApi(token=api_key)
+def main_loop(configuration):
+    # setup connection to VK API
+    vk_session = vk_api.VkApi(token=configuration.get_api_key())
     session_api = vk_session.get_api()
-    longpoll = VkBotLongPoll(vk_session, group_id, 100)
+    longpoll = VkBotLongPoll(vk_session, configuration.get_group_id(), 100)
 
-    return session_api, longpoll
-
-
-def get_subscribers():
-    with open(SUBSCRIPTION_ID_FILE_NAME, "r") as user_ids:
-        return [int(lne) for lne in user_ids]
-
-
-def get_messages_ids():
-    # read message id from where you should start
-    with open(LATEST_MSG_ID_FILE_NAME, "r") as msg_id:
-        return [int(lne) for lne in msg_id]
-
-
-def main_loop(connected_ids, session_api, longpoll):
     APP_LOGGER.info('Server started listening')
 
     while True:
@@ -70,34 +49,35 @@ def main_loop(connected_ids, session_api, longpoll):
             for event in longpoll.listen():
                 APP_LOGGER.info('Event handling start...')
                 if event.type == VkBotEventType.MESSAGE_NEW:
-                    # handle message from subscribed user
+                    # handle message from user
                     if event.from_user:
-                        # receive users id
-                        from_id = event.obj.message[u'from_id']
-                        APP_LOGGER.info(f'message from user with id {event.obj.message["from_id"]} received')
 
-                        # if from_id is new, we'll add it to special list, then write updated info to file
-                        if from_id not in connected_ids:
-                            APP_LOGGER.info('new user connected / record added')
-                            with open(SUBSCRIPTION_ID_FILE_NAME, "a") as f:
-                                f.write(f'\n{from_id}')
+                        # add user_id if not in subscriptions list
+                        from_id = event.obj.message[u'from_id']
+                        APP_LOGGER.info(f'message from user with id {event.obj.message["from_id"]} recieved')
+                        if from_id not in configuration.get_subscriber_ids():
+                            configuration.add_group_id(from_id)
+                            configuration.save_subscriber_ids()
+                            APP_LOGGER.info(f'user with id {event.obj.message["from_id"]} saved')
 
                         # mark message as read
                         session_api.messages.markAsRead(
                             start_message_id=[event.message['id']],
                             peer_id=event.message['peer_id'],
                         )
+                        APP_LOGGER.info(f'message from user with id {event.obj.message["from_id"]} marked as read')
 
                     # handle message from group chat
                     elif event.from_chat and event.message.text != '' and event.message.text[0:2] == '//':
                         APP_LOGGER.info('message from chat')
                         # notify subscribed users
-                        for userId in connected_ids:
-                            # switch 'peer_id' to broadcast list
-                            # forward messages not supported by VK API for bots??? W H A T !? :\
+                        for userId in configuration.get_subscriber_ids:
+                            
                             APP_LOGGER.info(reform_attachments(event.message.attachments))
                             APP_LOGGER.info(reform_forward_msg(event.message.fwd_messages))
-
+                            
+                            # TODO switch 'peer_id' to broadcast list
+                            # forward messages not supported by VK API for bots??? W H A T !? :\
                             session_api.messages.send(
                                 peer_id=userId,
                                 random_id=0,
@@ -108,6 +88,7 @@ def main_loop(connected_ids, session_api, longpoll):
                 APP_LOGGER.info('Event handling ended')
         except Exception as e:
             APP_LOGGER.exception(e)
+            pass
 
 
 def setup_logger():
@@ -137,6 +118,6 @@ def setup_logger():
 
 if __name__ == '__main__':
     setup_logger()
-    main_tread = threading.Thread(target=main_loop, args=(get_subscribers(), *load_settings()))
+    # why we need thread?
+    main_tread = threading.Thread(target=main_loop, args=(LegacyConfigurationLoader()))
     main_tread.start()
-
