2cd915ad3a0e75a7a986050ba56fcb47fb8f18f1
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-03-07 15:59:06 +0300

    code refactored

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ from strategies.legacy_strategy import LegacyStrategy
 
 APP_LOGGER = logging
 
-def main_loop(configuration):
-    # setup connection to VK API
-    vk_session = vk_api.VkApi(token=configuration.get_api_key())
-    session_api = vk_session.get_api()
-    longpoll = VkBotLongPoll(vk_session, configuration.get_group_id(), 100)
-
+def main_loop(configuration, strategy):
     APP_LOGGER.info('Server started listening')
-    strategy = LegacyStrategy(session_api, APP_LOGGER, configuration)
 
     while True:
         try:
+            APP_LOGGER.info('Event handling start...')
             for event in longpoll.listen():
-                APP_LOGGER.info('Event handling start...')
+                APP_LOGGER.info('Event catched')
                 strategy.handle_event(event)
         except Exception as e:
             APP_LOGGER.exception(e)
+            APP_LOGGER.info("Reestablishing server")
             pass
 
 
@@ -57,8 +53,15 @@ def setup_logger(fileFlag):
 
 
 if __name__ == '__main__':
-    setup_logger(True)
-    main_loop(LegacyConfigurationLoader())
-    # why we need thread?
-    # main_tread = threading.Thread(target=main_loop, args=([LegacyConfigurationLoader()]))
-    # main_tread.start()
+    setup_logger(fileFlag=True)
+
+    # setup configuration
+    configuration = LegacyConfigurationLoader()
+    # setup connection to VK API
+    vk_session = vk_api.VkApi(token=configuration.get_api_key())
+    session_api = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, configuration.get_group_id(), 100)
+    # setup strategy
+    strategy = LegacyStrategy(session_api, APP_LOGGER, configuration)
+    
+    main_loop(configuration, strategy)
